dbb35015552dcebcbd957361ce92081f65faca5b
Chris Bentivenga <chris@chrisbenti.com>
2019-07-14 21:57:44 -0700

    rip cors.io

--- src/services/SnokingSeason.ts
+++ src/services/SnokingSeason.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { SnokingSeasonResponse } from "../../typings/snokingData";
 
 export async function getSnokingSeasonData(url: string): Promise<SnokingSeasonResponse> {
-    return (await axios.get(`https://cors.io/?${url}`, {
+    return (await axios.get(`https://api.codetabs.com/v1/proxy?quest=${url}`, {
         headers: {
             accept: "application/json, text/plain, */*"
         }
