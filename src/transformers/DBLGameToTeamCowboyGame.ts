784a5428eeef21722426c98d12ef08679d396da2
Shawn Collinge <shawnc6@cs.washington.edu>
2023-06-18 22:23:03 -0700

    Added end time to teamcowboy and shirt colour

--- src/transformers/DBLGameToTeamCowboyGame.ts
+++ src/transformers/DBLGameToTeamCowboyGame.ts
@@ -3,9 +3,11 @@ import { DBLGame } from "../../typings/dblAppData";
 import moment from "moment";
 
 const BENCH_APP_DATE_FORMAT_STRING = "YYYY-MM-DD";
+const END_TIME_FORMAT = "hh:mm:SS A";
 
 export function DBLGameToTeamCowboyGame(snokingGame: SnokingGame, teamId: string): DBLGame {
     const isHome = teamId == `${snokingGame.teamHomeSeasonId}`;
+    const endTime = moment(snokingGame.dateTime).add(90, "minutes");
     return {
         Type: "GAME",
         Date: moment(snokingGame.dateTime).format(BENCH_APP_DATE_FORMAT_STRING),
@@ -13,5 +15,8 @@ export function DBLGameToTeamCowboyGame(snokingGame: SnokingGame, teamId: string
         Title: isHome ? snokingGame.teamAwayName : snokingGame.teamHomeName,
         HomeOrAway: isHome ? "Home" : "Away",
         rinkName: snokingGame.rinkName,
+        jerseyColour: isHome ? "White" : "Black",
+        endDate: moment(endTime).format(BENCH_APP_DATE_FORMAT_STRING),
+        endTime: moment(endTime).format(END_TIME_FORMAT),
     };
 }
