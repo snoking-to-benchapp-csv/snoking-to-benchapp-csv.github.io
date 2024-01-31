2fbfa603f1b1720615bece3170370b83f4025c97
Shawn Collinge <shawnc6@cs.washington.edu>
2024-01-31 10:49:01 -0800

    Remove seconds from end time

--- src/transformers/DBLGameToTeamCowboyGame.ts
+++ src/transformers/DBLGameToTeamCowboyGame.ts
@@ -2,21 +2,21 @@ import { SnokingGame } from "../../typings/snokingData";
 import { DBLGame } from "../../typings/dblAppData";
 import moment from "moment";
 
-const BENCH_APP_DATE_FORMAT_STRING = "YYYY-MM-DD";
-const END_TIME_FORMAT = "hh:mm:SS A";
+const TEAMCOWBOY_DATE_FORMAT_STRING = "YYYY-MM-DD";
+const END_TIME_FORMAT = "hh:mm A";
 
 export function DBLGameToTeamCowboyGame(snokingGame: SnokingGame, teamId: string): DBLGame {
     const isHome = teamId == `${snokingGame.teamHomeSeasonId}`;
     const endTime = moment(snokingGame.dateTime).add(75, "minutes");
     return {
         Type: "GAME",
-        Date: moment(snokingGame.dateTime).format(BENCH_APP_DATE_FORMAT_STRING),
+        Date: moment(snokingGame.dateTime).format(TEAMCOWBOY_DATE_FORMAT_STRING),
         Time: snokingGame.time,
         Title: isHome ? snokingGame.teamAwayName : snokingGame.teamHomeName,
         HomeOrAway: isHome ? "Home" : "Away",
         rinkName: snokingGame.rinkName,
         jerseyColour: isHome ? "White" : "Black",
-        endDate: moment(endTime).format(BENCH_APP_DATE_FORMAT_STRING),
+        endDate: moment(endTime).format(TEAMCOWBOY_DATE_FORMAT_STRING),
         endTime: moment(endTime).format(END_TIME_FORMAT),
     };
 }
